c47645f1df5346959a996c4842f3e728032871a0
Lance Kelley <lancekkcodes@gmail.com>
2021-11-07 01:41:26 -0600

    product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+    ],
+    include: [
+      Category,
+      Tag
+    ]
+  }).then(ps => {
+    console.log(ps);
+    res.json(ps);
+    return ps;
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+    ],
+    include: [
+      Category,
+      Tag
+    ]
+  }).then(ps => {
+    console.log(ps);
+    res.json(ps);
+    return ps;
+  });
 });
 
 // create new product
@@ -91,6 +122,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(v => {
+    res.json(v);
+  });
 });
 
 module.exports = router;
