4801c4677f09525c81ccd00e2398255007d4a551
Lance Kelley <lancekkcodes@gmail.com>
2021-11-07 17:16:42 -0600

    Tag Routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -40,14 +40,40 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  }).then(tag => {
+    res.status(200).json(tag);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name,
+  }, {
+    where: {id: req.params.id},
+  }).then(t => {
+    if (t === 0) {
+      res.status(404).json({});
+    }
+    res.status(200).json(t);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {id: req.params.id}
+  }).then(t => {
+    res.status(200).json(t);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
