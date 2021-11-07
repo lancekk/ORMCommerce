1299b20881c9d066d55e3c5ce9cf4e88a5e81716
Lance Kelley <lancekkcodes@gmail.com>
2021-11-07 17:32:13 -0600

    category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,76 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  }).then(cats => {
+    res.status(200).json(cats);
+    return cats;
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {id: req.params.id},
+    include: [Product],
+  }).then(cat => {
+    if (cat) {
+      res.status(200).json(cat);
+    } else {
+      res.status(404).json({});
+    }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  }).then(cat => {
+    res.status(200).json(cat);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).send(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name,
+  }, {
+    where: {id: req.params.id},
+  }).then(num => {
+    if (num[0] !== 0) {
+      res.status(200).json(num);
+    } else {
+      res.status(404).json({});
+    }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).send("");
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {id: req.params.id}
+  }).then(num => {
+    if (num !== 0) {
+      res.status(200).json(num);
+    } else {
+      res.status(404).json({});
+    }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).send("");
+  });
 });
 
 module.exports = router;
